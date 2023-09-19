fda17094d7b1809e54cf35283c6e6997561f3f3c
FGiribone <fabricio.giribone@gmail.com>
2023-09-19 10:24:20 -0300

    actualizacion

--- js/Producto.js
+++ js/Producto.js
@@ -1,19 +1,37 @@
 class Producto{
-    constructor(descripcion,codigo,precio,urlImagen,categoria){
+    constructor(descripcion,codigo,precio,stock,urlImagen,categoria){
         this.descripcion = descripcion;
         this.codigo = codigo;
         this.precio = precio;
+        this.stock = stock;
         this.urlimagen = urlImagen;
         this.categoria = categoria;
     }
 
-    obtenerCodigo(){
+    // GETS
+    getDescripcion(){
+        console.log('Descripcion: ${this.descripcion')
+    }
+    getCodigo(){
+        console.log('Codigo: ${this.Codigo')
+    }
+    getPrecio(){
+        console.log('Precio: ${this.precio')
+    }
+    getStock(){
+        console.log('Stock: ${this.stock')
+    }
+    getCategoria(){
+        console.log('Categoria: ${this.categoria')
+    }
+
+    mostrarCodigo(){
     console.log('Descripción: ${this.nombre}');
     console.log('precio: ${this.precio}');
     console.log('codigo: ${this.codigo}');
     console.log('Categoria:${this.categoria.nombre}')
     }
-
+    
     mostrarImagen() {
         const imagenElement = document.createElement('img');
         imagenElement.src = this.urlImagen;
